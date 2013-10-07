3ee71d116ce2b1f318e5ab24eaa1d2e385b47e63
dermetfan <serverkorken@googlemail.com>
2013-10-07 19:46:07 +0200

    added MouseJointAdapter

--- libgdx-utils/src/net/dermetfan/util/libgdx/CutsceneManager.java
+++ libgdx-utils/src/net/dermetfan/util/libgdx/CutsceneManager.java
@@ -1,11 +1,25 @@
+/**
+ * Copyright 2013 Robin Stumm (serverkorken@googlemail.com, http://dermetfan.bplaced.net)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package net.dermetfan.util.libgdx;
 
 import com.badlogic.gdx.utils.Array;
 
-/**
- * Manages {@link Cutscene Cutscenes}
- * @author dermetfan
- */
+/** Manages {@link Cutscene Cutscenes}
+ *  @author dermetfan */
 public class CutsceneManager {
 
 	/** the currently active {@link Cutscene Cutscenes} */
@@ -40,11 +54,9 @@ public class CutsceneManager {
 		/** called by {@link CutsceneManager#start(Cutscene)} */
 		public void init();
 
-		/**
-		 * called by {@link CutsceneManager#update(float)}
-		 * @param delta the time passes since last update
-		 * @return if the cutscene is finished
-		 */
+		/** called by {@link CutsceneManager#update(float)}
+		 *  @param delta the time passes since last update
+		 *  @return if the cutscene is finished */
 		public boolean update(float delta);
 
 		/** called by {@link CutsceneManager#end(Cutscene)} */
