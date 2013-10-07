d093135d58b91c045e35efe794e4f890a2cd2a0b
Robin Stumm <dermetfan@users.noreply.github.com>
2013-10-07 04:42:26 +0200

    Breakable is now usable

--- libgdx-utils/src/net/dermetfan/util/libgdx/box2d/Breakable.java
+++ libgdx-utils/src/net/dermetfan/util/libgdx/box2d/Breakable.java
@@ -3,6 +3,7 @@ package net.dermetfan.util.libgdx.box2d;
 import net.dermetfan.util.Accessor;
 import net.dermetfan.util.math.MathUtils;
 
+import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.Contact;
 import com.badlogic.gdx.physics.box2d.ContactImpulse;
 import com.badlogic.gdx.physics.box2d.ContactListener;
@@ -10,17 +11,52 @@ import com.badlogic.gdx.physics.box2d.Fixture;
 import com.badlogic.gdx.physics.box2d.Manifold;
 import com.badlogic.gdx.utils.Array;
 
-public class Breakable implements ContactListener {
+/**
+ * Breaks bodies or fixtures if they get hit too hard. Put in fixture's or body's user data and set {@link Notifier} as {@link ContactListener}.<br/>
+ * Don't forget to call {@link #destroy()} after every time step.<br/>
+ * You can manually destroy fixtures or bodies using the {@link #destroy(Fixture, boolean, boolean)} and {@link #destroy(Body)} methods.
+ * @author dermetfan
+ */
+public class Breakable {
 
-	/** how much force the breakable can bear */
-	private float robustness;
+	/** calls {@link Breakable#strain(Contact, ContactImpulse)} in {@link ContactListener#postSolve(Contact, ContactImpulse) postSolve} */
+	public static class Notifier implements ContactListener {
 
-	/** creates a new Breakable with the given {@link #robustness} */
-	public Breakable(float robustness) {
-		this.robustness = robustness;
+		@Override
+		public void beginContact(Contact contact) {
+		}
+
+		@Override
+		public void preSolve(Contact contact, Manifold oldManifold) {
+		}
+
+		@Override
+		public void postSolve(Contact contact, ContactImpulse impulse) {
+			strain(contact, impulse);
+		}
+
+		@Override
+		public void endContact(Contact contact) {
+		}
+
 	}
 
-	/** The {@link Accessor} used to access user data. */
+	/** how much force the Breakable can bear */
+	private float resistance;
+
+	/** if the fixture's body (in case the Breakable is used for a fixture) should be destroyed if the fixture is destroyed (false by default) */
+	private boolean breakBody;
+
+	/** if the fixture's body (in case the Breakable is used for a fixture) should be destroyed if the fixture is destroyed and it was the bodie's last one (true by default) */
+	private boolean breakBodyWithoutFixtures = true;
+
+	/** the fixtures that broke in {@link #strain(Contact, ContactImpulse)} */
+	private static final Array<Fixture> brokenFixtures = new Array<Fixture>(0);
+
+	/** the bodies that broke in {@link #strain(Contact, ContactImpulse)} */
+	private static final Array<Body> brokenBodies = new Array<Body>(0);
+
+	/** the {@link Accessor} used to access user data */
 	private static Accessor userDataAccessor = new Accessor() {
 
 		@SuppressWarnings("unchecked")
@@ -31,53 +67,110 @@ public class Breakable implements ContactListener {
 
 	};
 
-	/** the fixtures that broke in the last timestep */
-	private static final Array<Fixture> brokenFixtures = new Array<Fixture>(0);
+	/** creates a new Breakable with the given {@link #resistance} */
+	public Breakable(float robustness) {
+		resistance = robustness;
+	}
 
-	public static void update() {
+	/** destroys all bodies in {@link #brokenBodies} and fixtures in {@link #brokenFixtures} */
+	public static void destroy() {
 		for(Fixture fixture : brokenFixtures) {
 			brokenFixtures.removeValue(fixture, true);
-			fixture.getBody().destroyFixture(fixture); // TODO if body has no fixtures left, destroy it? is that desired?
+			fixture.getBody().destroyFixture(fixture);
+		}
+		for(Body body : brokenBodies) {
+			brokenBodies.removeValue(body, true);
+			body.getWorld().destroyBody(body);
 		}
 	}
 
-	@Override
-	public void beginContact(Contact contact) {
+	/** {@link #destroy(Fixture, boolean, boolean) destroys}/{@link #destroy(Body) destroys} all fixtures/bodies involved in the given Contact if they could not bear the given impulse */
+	public static void strain(Contact contact, ContactImpulse impulse) {
+		Breakable breakable;
+		float impulseSum = MathUtils.sum(impulse.getNormalImpulses());
+
+		Fixture fixtureA = contact.getFixtureA(), fixtureB = contact.getFixtureB();
+		if((breakable = userDataAccessor.access(fixtureA.getUserData())) != null && impulseSum > breakable.resistance)
+			destroy(fixtureA, breakable.breakBodyWithoutFixtures, breakable.breakBody);
+
+		if((breakable = userDataAccessor.access(fixtureB.getUserData())) != null && impulseSum > breakable.resistance)
+			destroy(fixtureB, breakable.breakBodyWithoutFixtures, breakable.breakBody);
+
+		Body bodyA = fixtureA.getBody(), bodyB = fixtureB.getBody();
+		if((breakable = userDataAccessor.access(bodyA.getUserData())) != null && impulseSum > breakable.resistance)
+			destroy(bodyA);
+
+		if((breakable = userDataAccessor.access(bodyB.getUserData())) != null && impulseSum > breakable.resistance)
+			destroy(bodyB);
 	}
 
-	@Override
-	public void endContact(Contact contact) {
+	/** destroys the given fixture (and its body depending on breakBodyWithoutFixtures and breakBody)
+	 *  @param fixture the {@link Fixture} to destroy
+	 *  @param breakBodyWithoutFixtures {@link #breakBodyWithoutFixtures}
+	 *  @param breakBody {@link #breakBody} */
+	public static void destroy(Fixture fixture, boolean breakBodyWithoutFixtures, boolean breakBody) {
+		if(!brokenFixtures.contains(fixture, true))
+			brokenFixtures.add(fixture);
+
+		Body body = fixture.getBody();
+		if(brokenBodies.contains(body, true))
+			return;
+
+		Array<Fixture> bodyFixtureList = body.getFixtureList();
+
+		int brokenFixturesOfBody = 1;
+		for(Fixture fixt : brokenFixtures)
+			if(bodyFixtureList.contains(fixt, true))
+				brokenFixturesOfBody++;
+
+		if(breakBodyWithoutFixtures && bodyFixtureList.size <= brokenFixturesOfBody || breakBody)
+			destroy(body);
 	}
 
-	@Override
-	public void preSolve(Contact contact, Manifold oldManifold) {
+	/** @param body the {@link Body} to destroy */
+	public static void destroy(Body body) {
+		if(!brokenBodies.contains(body, true))
+			brokenBodies.add(body);
 	}
 
-	@Override
-	public void postSolve(Contact contact, ContactImpulse impulse) {
-		// TODO break bodies as well
-
-		Fixture a = contact.getFixtureA(), b = contact.getFixtureB();
-		if(userDataAccessor.access(a.getUserData()) != null && MathUtils.sum(impulse.getNormalImpulses()) > robustness && !brokenFixtures.contains(a, true))
-			brokenFixtures.add(a);
-
-		if(userDataAccessor.access(b.getUserData()) != null && MathUtils.sum(impulse.getNormalImpulses()) > robustness && !brokenFixtures.contains(b, true))
-			brokenFixtures.add(b);
+	/** @return the {@link #resistance} */
+	public float getResistance() {
+		return resistance;
 	}
 
-	/** @return the {@link #robustness} */
-	public float getRobustness() {
-		return robustness;
+	/** @param resistance the {@link #resistance} to set */
+	public void setResistance(float resistance) {
+		this.resistance = resistance;
 	}
 
-	/** @param robustness the {@link #robustness} to set */
-	public void setRobustness(float robustness) {
-		this.robustness = robustness;
+	/** @return the {@link #breakBody} */
+	public boolean isBreakBody() {
+		return breakBody;
+	}
+
+	/** @param breakBody the {@link #breakBody} to set */
+	public void setBreakBody(boolean breakBody) {
+		this.breakBody = breakBody;
+	}
+
+	/** @return the {@link #breakBodyWithoutFixtures} */
+	public boolean isBreakBodyWithoutFixtures() {
+		return breakBodyWithoutFixtures;
+	}
+
+	/** @param breakBodyWithoutFixtures the {@link #breakBodyWithoutFixtures} to set */
+	public void setBreakBodyWithoutFixtures(boolean breakBodyWithoutFixtures) {
+		this.breakBodyWithoutFixtures = breakBodyWithoutFixtures;
 	}
 
 	/** @return the {@link #brokenFixtures} */
 	public static Array<Fixture> getBrokenFixtures() {
 		return brokenFixtures;
+	}
+
+	/** @return the {@link #brokenBodies} */
+	public static Array<Body> getBrokenBodies() {
+		return brokenBodies;
 	}
 
 	/** @return the {@link #userDataAccessor} */
