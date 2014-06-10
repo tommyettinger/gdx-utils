5ca79dcda5293c0763794814e521718f46fab648
Robin Stumm <dermetfan@users.noreply.github.com>
2014-06-10 08:26:58 +0200

    fixed polygon vertice amount check

--- box2d/src/main/net/dermetfan/utils/libgdx/box2d/Box2DMapObjectParser.java
+++ box2d/src/main/net/dermetfan/utils/libgdx/box2d/Box2DMapObjectParser.java
@@ -636,7 +636,7 @@ public class Box2DMapObjectParser {
 
 		Polygon polygon;
 
-		if(!(mapObject instanceof PolygonMapObject) || isConvex(polygon = ((PolygonMapObject) mapObject).getPolygon()) && (!Box2DUtils.checkPreconditions || polygon.getVertices().length <= Box2DUtils.maxPolygonVertices))
+		if(!(mapObject instanceof PolygonMapObject) || isConvex(polygon = ((PolygonMapObject) mapObject).getPolygon()) && (!Box2DUtils.checkPreconditions || polygon.getVertices().length / 2 <= Box2DUtils.maxPolygonVertices))
 			return new Fixture[] {createFixture(mapObject, body)};
 
 		Polygon[] convexPolygons = triangulate ? triangulate(polygon) : decompose(polygon);
