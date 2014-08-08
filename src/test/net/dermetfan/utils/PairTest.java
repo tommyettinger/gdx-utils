45045e7568833e67662a6b18227fd54762e1692b
dermetfan <serverkorken@gmail.com>
2014-08-08 06:41:47 +0200

    added PairTest#isFull() and isEmpty()

--- src/test/net/dermetfan/utils/PairTest.java
+++ src/test/net/dermetfan/utils/PairTest.java
@@ -10,6 +10,37 @@ public class PairTest {
 		Pair<String, String> pair = new Pair<>("1", "2");
 		pair.swap();
 		assertEquals(new Pair<>("2", "1"), pair);
+		assertTrue(pair.isFull());
+		assertFalse(pair.isEmpty());
+		pair.value(null);
+		assertFalse(pair.isFull());
+		assertFalse(pair.isEmpty());
+		assertFalse(pair.hasValue());
+		assertTrue(pair.hasKey());
+		pair.clear();
+		assertTrue(pair.isEmpty());
+		assertFalse(pair.isFull());
+		assertFalse(pair.hasKey());
+	}
+
+	@Test
+	public void isFull() {
+		Pair<String, String> pair = new Pair<>("1", "2");
+		assertTrue(pair.isFull());
+		pair.value(null);
+		assertFalse(pair.isFull());
+		pair.clear();
+		assertFalse(pair.isFull());
+	}
+
+	@Test
+	public void isEmpty() {
+		Pair<String, String> pair = new Pair<>("1", "2");
+		assertFalse(pair.isEmpty());
+		pair.value(null);
+		assertFalse(pair.isEmpty());
+		pair.clear();
+		assertTrue(pair.isEmpty());
 	}
 
 }
