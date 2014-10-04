539b3c310a1a51d46e41beead444dac535455d13
dermetfan <serverkorken@gmail.com>
2014-10-04 14:33:53 +0200

    added WorldObserver, added Box2DUtils in libGDX package hack, added DualObjectMap, added DualIntObjectMap, moved Box2DUtils and Box2DMapObjectParser

--- box2d/src/main/net/dermetfan/gdx/physics/box2d/Box2DUtils.java
+++ box2d/src/main/net/dermetfan/gdx/physics/box2d/Box2DUtils.java
@@ -12,7 +12,7 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License. */
 
-package net.dermetfan.gdx.physics.box2d.utils;
+package net.dermetfan.gdx.physics.box2d;
 
 import com.badlogic.gdx.math.Intersector;
 import com.badlogic.gdx.math.Vector2;
