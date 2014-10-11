719e3434443e62a66ded0081f4d1d97e405cecd5
dermetfan <serverkorken@gmail.com>
2014-10-11 02:33:37 +0200

    incremented version to 0.6.0, added Dual[Int/Object]Map constructors, javadoc

--- src/main/net/dermetfan/gdx/utils/DualObjectMap.java
+++ src/main/net/dermetfan/gdx/utils/DualObjectMap.java
@@ -16,46 +16,71 @@ package net.dermetfan.gdx.utils;
 
 import com.badlogic.gdx.utils.ObjectMap;
 
-/** two {@link ObjectMap ObjectMaps} holding each others contents in reverse for fast retrieval of both keys and values
+/** Two {@link ObjectMap ObjectMaps} holding each others contents in reverse for fast retrieval of both keys and values.
+ *  This causes null values not to be allowed.
  *  @since 0.5.1
  *  @author dermetfan */
 public class DualObjectMap<K, V> {
 
+	/** the map holding keys as keys */
 	private final ObjectMap<K, V> keyValue;
+
+	/** the map holding values as keys */
 	private final ObjectMap<V, K> valueKey;
 
+	/** @see ObjectMap#ObjectMap() */
 	public DualObjectMap() {
 		keyValue = new ObjectMap<>();
 		valueKey = new ObjectMap<>();
 	}
 
+	/** @see ObjectMap#ObjectMap(int) */
 	public DualObjectMap(int initialCapacity) {
 		keyValue = new ObjectMap<>(initialCapacity);
 		valueKey = new ObjectMap<>(initialCapacity);
 	}
 
+	/** @see ObjectMap#ObjectMap(int, float) */
 	public DualObjectMap(int initialCapacity, float loadFactor) {
 		keyValue = new ObjectMap<>(initialCapacity, loadFactor);
 		valueKey = new ObjectMap<>(initialCapacity, loadFactor);
 	}
 
+	/** @see ObjectMap#ObjectMap(ObjectMap) */
+	public DualObjectMap(ObjectMap<K, V> map) {
+		keyValue = new ObjectMap<>(map);
+		valueKey = new ObjectMap<>(map.size);
+		for(K key : map.keys())
+			valueKey.put(map.get(key), key);
+	}
+
+	/** @param map the map to copy */
+	public DualObjectMap(DualObjectMap<K, V> map) {
+		keyValue = new ObjectMap<>(map.keyValue);
+		valueKey = new ObjectMap<>(map.valueKey);
+	}
+
+	/** @see ObjectMap#put(Object, Object) */
 	public void put(K key, V value) {
 		keyValue.put(key, value);
 		valueKey.put(value, key);
 	}
 
+	/** @return the key of the given value as {@link ObjectMap#findKey(Object, boolean)} would return */
 	public K getKey(V value) {
 		K key = valueKey.get(value);
 		assert key != null;
 		return key;
 	}
 
+	/** @see ObjectMap#get(Object) */
 	public V getValue(K key) {
 		V value = keyValue.get(key);
 		assert value != null;
 		return value;
 	}
 
+	/** @see ObjectMap#remove(Object) */
 	public V removeKey(K key) {
 		V value = keyValue.remove(key);
 		assert value != null;
@@ -64,6 +89,7 @@ public class DualObjectMap<K, V> {
 		return value;
 	}
 
+	/** like what {@code objectMap.remove(objectMap.findKey(value))} would do */
 	public K removeValue(V value) {
 		K key = valueKey.remove(value);
 		assert key != null;
