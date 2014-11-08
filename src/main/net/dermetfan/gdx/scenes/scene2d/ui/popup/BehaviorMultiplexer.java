b60701f7dd04b590f6912c24765b0805144c92e1
dermetfan <serverkorken@gmail.com>
2014-11-08 18:59:49 +0100

    whole bunch of stuff, Tooltip removed (needs to be reimplemented)

---
+++ src/main/net/dermetfan/gdx/scenes/scene2d/ui/popup/BehaviorMultiplexer.java
@@ -0,0 +1,84 @@
+/** Copyright 2014 Robin Stumm (serverkorken@gmail.com, http://dermetfan.net)
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License. */
+
+package net.dermetfan.gdx.scenes.scene2d.ui.popup;
+
+import com.badlogic.gdx.scenes.scene2d.Event;
+import com.badlogic.gdx.utils.Array;
+import net.dermetfan.gdx.Multiplexer;
+import net.dermetfan.gdx.scenes.scene2d.ui.popup.Popup.Behavior;
+
+/** a Multiplexer for Behaviors
+ *  @since 0.8.0
+ *  @author dermetfan */
+public class BehaviorMultiplexer extends Multiplexer<Behavior> implements Behavior {
+
+	public BehaviorMultiplexer() {}
+
+	public BehaviorMultiplexer(int size) {
+		super(size);
+	}
+
+	public BehaviorMultiplexer(Behavior... receivers) {
+		super(receivers);
+	}
+
+	public BehaviorMultiplexer(Array<Behavior> receivers) {
+		super(receivers);
+	}
+
+	/** @return whether any of the Behaviors handled the event */
+	@Override
+	public boolean show(Event event, Popup popup) {
+		boolean handled = false;
+		for(Behavior behavior : receivers)
+			handled |= behavior.show(event, popup);
+		return handled;
+	}
+
+	/** @return whether any of the Behaviors handled the event */
+	@Override
+	public boolean hide(Event event, Popup popup) {
+		boolean handled = false;
+		for(Behavior behavior : receivers)
+			handled |= behavior.hide(event, popup);
+		return handled;
+	}
+
+	/** @return the first Reaction received from the last Behavior combined with Reaction.Handle if any Behavior handles the event */
+	@Override
+	public Reaction handle(Event event, Popup popup) {
+		Reaction reaction = null;
+		int i = receivers.size - 1;
+		for(; reaction == null && i >= 0; i--)
+			reaction = receivers.get(i).handle(event, popup);
+		if(reaction != Reaction.ShowHandle && reaction != Reaction.HideHandle && reaction != Reaction.Handle) {
+			for(; i >= 0; i--) {
+				Reaction react = receivers.get(i).handle(event, popup);
+				if(react == Reaction.ShowHandle || react == Reaction.HideHandle || react == Reaction.Handle)
+					switch(reaction != null ? reaction : Reaction.None) {
+					case Show:
+						return Reaction.ShowHandle;
+					case Hide:
+						return Reaction.HideHandle;
+					case None:
+					default:
+						return Reaction.Handle;
+					}
+			}
+		}
+		return reaction;
+	}
+
+}
---
+++ src/main/net/dermetfan/gdx/scenes/scene2d/ui/popup/ContextMenuBehavior.java
@@ -0,0 +1,72 @@
+/** Copyright 2014 Robin Stumm (serverkorken@gmail.com, http://dermetfan.net)
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License. */
+
+package net.dermetfan.gdx.scenes.scene2d.ui.popup;
+
+import com.badlogic.gdx.Input.Buttons;
+import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.Event;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import net.dermetfan.gdx.scenes.scene2d.EventMultiplexer;
+
+/** The behavior of a classic context menu. Add this to the Popup of the actor that the user should be able to right-click.
+ *  <strong>Note that this can only hide on events of other actors if it receives them, so consider adding all your context menus to an {@link EventMultiplexer} high up in the hierarchy (e.g. the {@link com.badlogic.gdx.scenes.scene2d.Stage Stage}).</strong>
+ *  @author dermetfan
+ *  @since 0.8.0 */
+public class ContextMenuBehavior extends BasicBehavior {
+
+	/** The Actor this context menu is added to. Needed to hide on events on other Actors. */
+	private Actor target;
+
+	/** @param target the {@link #target} */
+	public ContextMenuBehavior(Actor target) {
+		this.target = target;
+	}
+
+	/** {@link Reaction#ShowHandle Shows} on right click and menu key press. Hides on left click, escape key and back key.
+	 * 	Note that this will not hide on clicks on other actors except the {@link Event#getListenerActor()}'s children. */
+	@Override
+	public Reaction handle(Event e, Popup popup) {
+		if(!(e instanceof InputEvent))
+			return Reaction.None;
+		InputEvent event = (InputEvent) e;
+		switch(event.getType()) {
+		case touchDown:
+			if(event.getButton() == Buttons.RIGHT && event.getTarget() == target)
+				return Reaction.ShowHandle; // right click shows
+			else if(!Popup.isAscendantOf(popup, event.getTarget())) // don't hide on clicks on this or child popups
+				return Reaction.Hide; // other clicks hide
+		case keyDown:
+			if(event.getKeyCode() == Keys.MENU && event.getTarget() == target) // menu key shows
+				return Reaction.ShowHandle;
+			else if(event.getKeyCode() == Keys.ESCAPE || event.getKeyCode() == Keys.BACK) // escape and back hide
+				return Reaction.HideHandle;
+		}
+		return Reaction.None;
+	}
+
+	// getters and setters
+
+	/** @return the {@link #target} */
+	public Actor getTarget() {
+		return target;
+	}
+
+	/** @param target the {@link #target} to set */
+	public void setTarget(Actor target) {
+		this.target = target;
+	}
+
+}
