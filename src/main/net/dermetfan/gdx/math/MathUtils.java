f54f4598cd23b916110ce0ce287a52bcc9d26df7
dermetfan <serverkorken@gmail.com>
2015-01-28 05:13:57 +0100

    minor simplification

--- src/main/net/dermetfan/gdx/math/MathUtils.java
+++ src/main/net/dermetfan/gdx/math/MathUtils.java
@@ -24,73 +24,73 @@ public class MathUtils extends net.dermetfan.utils.math.MathUtils {
 
 	/** @see net.dermetfan.utils.math.MathUtils#clamp(float[], float, float, int, int) */
 	public static FloatArray clamp(FloatArray values, float min, float max) {
-		net.dermetfan.utils.math.MathUtils.clamp(values.items, min, max, 0, values.size);
+		clamp(values.items, min, max, 0, values.size);
 		return values;
 	}
 
 	/** @see net.dermetfan.utils.math.MathUtils#abs(float[], int, int) */
 	public static FloatArray abs(FloatArray values) {
-		net.dermetfan.utils.math.MathUtils.abs(values.items, 0, values.size);
+		abs(values.items, 0, values.size);
 		return values;
 	}
 
 	/** @see net.dermetfan.utils.math.MathUtils#add(float[], float, int, int) */
 	public static FloatArray add(FloatArray values, float value) {
-		net.dermetfan.utils.math.MathUtils.add(values.items, value, 0, values.size);
+		add(values.items, value, 0, values.size);
 		return values;
 	}
 
 	/** @see net.dermetfan.utils.math.MathUtils#sub(float[], float, int, int) */
 	public static FloatArray sub(FloatArray values, float value) {
-		net.dermetfan.utils.math.MathUtils.sub(values.items, value, 0, values.size);
+		sub(values.items, value, 0, values.size);
 		return values;
 	}
 
 	/** @see net.dermetfan.utils.math.MathUtils#mul(float[], float, int, int) */
 	public static FloatArray mul(FloatArray values, float factor) {
-		net.dermetfan.utils.math.MathUtils.mul(values.items, factor, 0, values.size);
+		mul(values.items, factor, 0, values.size);
 		return values;
 	}
 
 	/** @see net.dermetfan.utils.math.MathUtils#div(float[], float, int, int) */
 	public static FloatArray div(FloatArray values, float divisor) {
-		net.dermetfan.utils.math.MathUtils.div(values.items, divisor, 0, values.size);
+		div(values.items, divisor, 0, values.size);
 		return values;
 	}
 
 	/** @see net.dermetfan.utils.math.MathUtils#sum(float[], int, int) */
 	public static float sum(FloatArray values) {
-		return net.dermetfan.utils.math.MathUtils.sum(values.items, 0, values.size);
+		return sum(values.items, 0, values.size);
 	}
 
 	/** @see net.dermetfan.utils.math.MathUtils#amplitude(float[], int, int) */
 	public static float amplitude(FloatArray f) {
-		return net.dermetfan.utils.math.MathUtils.amplitude(f.items, 0, f.size);
+		return amplitude(f.items, 0, f.size);
 	}
 
 	/** @see net.dermetfan.utils.math.MathUtils#max(float[], int, int) */
 	public static float max(FloatArray floats) {
-		return net.dermetfan.utils.math.MathUtils.max(floats.items, 0, floats.size);
+		return max(floats.items, 0, floats.size);
 	}
 
 	/** @see net.dermetfan.utils.math.MathUtils#min(float[], int, int) */
 	public static float min(FloatArray floats) {
-		return net.dermetfan.utils.math.MathUtils.min(floats.items, 0, floats.size);
+		return min(floats.items, 0, floats.size);
 	}
 
 	/** @see net.dermetfan.utils.math.MathUtils#nearest(float, float[], float, int, int) */
 	public static float nearest(float value, FloatArray values, float range) {
-		return net.dermetfan.utils.math.MathUtils.nearest(value, values.items, range, 0, values.size);
+		return nearest(value, values.items, range, 0, values.size);
 	}
 
 	/** @see net.dermetfan.utils.math.MathUtils#nearest(float, float[], int, int) */
 	public static float nearest(float value, FloatArray values) {
-		return net.dermetfan.utils.math.MathUtils.nearest(value, values.items, 0, values.size);
+		return nearest(value, values.items, 0, values.size);
 	}
 
 	/** @see net.dermetfan.utils.math.MathUtils#scale(float[], float, float, int, int) */
 	public static FloatArray scale(FloatArray values, float min, float max) {
-		net.dermetfan.utils.math.MathUtils.scale(values.items, min, max, 0, values.size);
+		scale(values.items, min, max, 0, values.size);
 		return values;
 	}
 
