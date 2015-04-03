8bc6d45706efdb00c78bc8041912c47344971365
Robin Stumm <dermetfan@users.noreply.github.com>
2015-04-03 03:43:02 +0200

    javadoc correction

--- box2d/src/main/net/dermetfan/gdx/graphics/g2d/Box2DSprite.java
+++ box2d/src/main/net/dermetfan/gdx/graphics/g2d/Box2DSprite.java
@@ -38,7 +38,7 @@ import static net.dermetfan.gdx.physics.box2d.Box2DUtils.position;
 import static net.dermetfan.gdx.physics.box2d.Box2DUtils.width;
 
 /** A {@link Box2DSprite} is a {@link Sprite} with additional drawing information and the ability to draw itself on a given {@link Body} or {@link Fixture}.
- *  It is supposed to be put in the user data of {@link Fixture Fixtures} or {@link Body Bodies}. Because geometrical information about bodies cannot be cached, it is faster to put Box2DPolygonSprites in the user data of Fixtures.
+ *  It is supposed to be put in the user data of {@link Fixture Fixtures} or {@link Body Bodies}. Because geometrical information about bodies cannot be cached, it is faster to put Box2DSprites in the user data of Fixtures.
  *  @author dermetfan */
 public class Box2DSprite extends Sprite {
 
