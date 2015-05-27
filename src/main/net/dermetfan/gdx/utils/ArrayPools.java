35d2cfd23a35a18298d5b7c15f5af28d69222017
Robin Stumm <dermetfan@users.noreply.github.com>
2015-05-27 13:46:43 +0200

    better primitive pools integration in ArrayPools

--- src/main/net/dermetfan/gdx/utils/ArrayPools.java
+++ src/main/net/dermetfan/gdx/utils/ArrayPools.java
@@ -21,6 +21,7 @@ import com.badlogic.gdx.utils.ObjectMap;
  *  @since 0.11.1 */
 public class ArrayPools {
 
+	/** holds pools by type */
 	private static final ObjectMap<Class, ArrayPool> typePools = new ObjectMap<>();
 
 	/** a static pool for float arrays */
@@ -28,32 +29,6 @@ public class ArrayPools {
 
 	/** a static pool for int arrays */
 	private static IntArrayPool intPool;
-
-	/** @return the {@link #floatPool} */
-	public static FloatArrayPool getFloatPool(int max, int maxEach) {
-		if(floatPool == null)
-			floatPool = new FloatArrayPool(max, maxEach);
-		return floatPool;
-	}
-
-	/** @return the {@link #intPool} */
-	public static IntArrayPool getIntPool(int max, int maxEach) {
-		if(intPool == null)
-			intPool = new IntArrayPool(max, maxEach);
-		return intPool;
-	}
-
-	/** calls {@link #getFloatPool(int, int)} with a max size of -1 (no max size) and a maxEach size of 100
-	 *  @see #getFloatPool(int, int) */
-	public static FloatArrayPool getFloatPool() {
-		return getFloatPool(-1, 100);
-	}
-
-	/** calls {@link #getIntPool(int, int)} with a max size of -1 (no max size) and a maxEach size of 100
-	 *  @see #getIntPool(int, int) */
-	public static IntArrayPool getIntPool() {
-		return getIntPool(-1, 100);
-	}
 
 	/** @param max note the max size is ignored if this is not the first time this pool has been requested
 	 *  @return a new {@link ReflectionArrayPool} or existing pool for the specified type, stored in a Class to {@link ArrayPool} map */
@@ -67,10 +42,34 @@ public class ArrayPools {
 		return pool;
 	}
 
+	/** @see #get(Class, int, int) */
+	public static FloatArrayPool getFloats(int max, int maxEach) {
+		if(floatPool == null)
+			floatPool = new FloatArrayPool(max, maxEach);
+		return floatPool;
+	}
+
+	/** @see #get(Class, int, int) */
+	public static IntArrayPool getInts(int max, int maxEach) {
+		if(intPool == null)
+			intPool = new IntArrayPool(max, maxEach);
+		return intPool;
+	}
+
 	/** calls {@link #get(Class, int, int)} with a max size of -1 (no max size) and a maxEach size of 100
 	 *  @see #get(Class, int, int) */
 	public static <T> ArrayPool<T> get(Class<T> type) {
 		return get(type, -1, 100);
+	}
+
+	/** @see #get(Class) */
+	public static FloatArrayPool getFloats() {
+		return getFloats(-1, 100);
+	}
+
+	/** @see #get(Class) */
+	public static IntArrayPool getInts() {
+		return getInts(-1, 100);
 	}
 
 	/** @param type the type for which to set the pool in a Class to {@link ArrayPool} map
@@ -79,9 +78,31 @@ public class ArrayPools {
 		typePools.put(type, pool);
 	}
 
+	/** @see #set(Class, ArrayPool) */
+	public static void set(FloatArrayPool pool) {
+		floatPool = pool;
+	}
+
+	/** @see #set(Class, ArrayPool) */
+	public static void set(IntArrayPool pool) {
+		intPool = pool;
+	}
+
 	/** @see ArrayPool#obtain(int) */
 	public static <T> T[] obtain(Class<T> type, int length) {
+		if(type.isPrimitive())
+			throw new IllegalArgumentException("Cannot return primitive array without boxing. Use obtain" + (type == float.class ? "Float" : type == int.class ? "Int" : "[Type]") + "s(int) instead.");
 		return get(type).obtain(length);
+	}
+
+	/** @see FloatArrayPool#obtain(int) */
+	public static float[] obtainFloats(int length) {
+		return getFloats().obtain(length);
+	}
+
+	/** @see IntArrayPool#obtain(int) */
+	public static int[] obtainInts(int length) {
+		return getInts().obtain(length);
 	}
 
 	/** @see ArrayPool#free(Object[]) */
@@ -91,6 +112,16 @@ public class ArrayPools {
 		@SuppressWarnings("unchecked")
 		ArrayPool<T> pool = (ArrayPool<T>) get(array.getClass().getComponentType());
 		pool.free(array);
+	}
+
+	/** @see FloatArrayPool#free(float[]) */
+	public static void free(float[] array) {
+		getFloats().free(array);
+	}
+
+	/** @see IntArrayPool#free(int[]) */
+	public static void free(int[] array) {
+		getInts().free(array);
 	}
 
 	private ArrayPools() {}
