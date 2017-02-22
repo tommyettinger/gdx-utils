863a325ab85be92342dbe828ce04ae11bcb78fb3
Robin Stumm <dermetfan@users.noreply.github.com>
2017-02-22 12:07:02 +0100

    add default.nix

---
+++ default.nix
@@ -0,0 +1,44 @@
+{ pkgs ? import <nixpkgs> {},
+  impureCache ? false }:
+
+pkgs.stdenv.mkDerivation {
+  name = "libgdx-utils";
+  src = ./.;
+
+  buildInputs = with pkgs; [
+    openjdk8
+  ];
+
+  buildPhase = ''
+    export GRADLE_USER_HOME=${if impureCache then "/tmp/.gradle" else ".gradle"}
+    ./gradlew build --no-daemon
+  '';
+
+  installPhase = ''
+    install_project() {
+      local build=$1
+      local name=$2
+    
+      mkdir -p $out/nix-support
+    
+      local doc=$out/share/doc/$name
+      mkdir -p $doc
+      mv $build/docs/javadoc/ $doc
+      mv $build/libs/*-javadoc.jar $doc
+      echo file $doc/*-javadoc.jar >> $out/nix-support/hydra-build-products
+
+      local src=$out/src
+      mkdir -p $src
+      mv $build/libs/*-sources.jar $src
+      echo file $src/*-sources.jar >> $out/nix-support/hydra-build-products
+
+      local lib=$out/lib
+      mkdir -p $lib
+      mv $build/libs/libgdx-utils-*.jar $lib
+      echo file binary-dist $lib/libgdx-utils-*.jar >> $out/nix-support/hydra-build-products
+    }
+
+    install_project build libgdx-utils
+    install_project box2d/build libgdx-utils-box2d
+  '';
+}
