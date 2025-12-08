a10a632654425bbf0153e735c3bf686c12ae522d
Tommy Ettinger <tommy.ettinger@gmail.com>
2025-12-07 18:13:33 -0800

    Tables need captions?

--- libgdx-utils/src/main/java/net/dermetfan/utils/math/MathUtils.java
+++ libgdx-utils/src/main/java/net/dermetfan/utils/math/MathUtils.java
@@ -91,6 +91,7 @@ public class MathUtils {
 
 	/** Normalizes/repeats the given value in the given interval [min, max] as if min and max were portals the value travels through. For example (min and max are both inclusive):<br>
 	 *  <table>
+	 *      <caption>examples</caption>
 	 *      <tr>
 	 *          <th>value</th>
 	 *          <th>min</th>
